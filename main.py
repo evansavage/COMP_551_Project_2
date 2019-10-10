75ef7e0e6aba23e5287d2a8cd96f3b0117409828
evansavage <esav678@gmail.com>
2019-10-10 17:36:52 -0400

    New LR pipeline testing

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import FeatureUnion, Pipeline
 from sklearn import svm
 import numpy as np
 import pandas as pd
 import datetime
 
-from dataset import load_dataset, dataset_analysis_extension, clean_dataset, add_pol_sub
+from dataset import load_dataset, dataset_analysis_extension, clean_dataset, \
+    add_pol_sub, pipelinize_feature, named_entry_recognition, get_polarity, \
+    get_subjectivity
 
 print('Is this run for tuning or for predicting?')
 tuning = input()
 # Load datasets
 
-count_vect = CountVectorizer()
-tfidf_transformer = TfidfTransformer(use_idf=True)
+count_vect = CountVectorizer(max_features=5000, ngram_range=(1,2))
+tfidf_transformer = TfidfTransformer(use_idf=True, norm='l2')
 
 original_dataset = load_dataset('reddit_train.csv', ',')
 test_dataset = load_dataset('reddit_test.csv', ',')
@@ -40,22 +42,30 @@ X_orig = clean_dataset(X_orig)
 # clf = MultinomialNB().fit(X, y)
 
 text_clf = Pipeline([
-    ('vect', count_vect),
-    ('tfidf', tfidf_transformer),
-    ('clf', LogisticRegression()),
+    ('features', FeatureUnion([
+        ('reg', Pipeline([
+            ('vect', count_vect),
+            ('tfidf', tfidf_transformer),
+        ])),
+        ('polarity', pipelinize_feature(get_polarity, active=True)),
+        ('subjectivity', pipelinize_feature(get_subjectivity, active=True)),
+    ])),
+    ('clf', LogisticRegression(multi_class='multinomial')),
 ])
 
 grid_params = {
-    'vect__max_df': (0.5, 0.75, 1.0),
-    'vect__max_features': (1500, 5000),
+    'features__reg__vect__max_df': (0.5, 0.75, 1.0),
+    'features__reg__vect__min_df': (1,2,3),
+    # 'vect__max_features': (5000, 10000),
     # 'tfidf__use_idf': (True, False),
     # 'tfidf__norm': ('l1', 'l2'),
     # 'clf__alpha': np.linspace(0.5, 1.5, 6), # For Naive Bayes
     # 'clf__fit_prior': [True, False], # For Naive Bayes
     # 'clf__decision_function_shape': ('ovo', 'ovr'), # For svm.SVC
-    # 'clf__max_iter': (500, 1000, 2000) # For svm.SVC
-    'clf__penalty': ('l1', 'l2'),
-    'clf__C': (0.2, 0.5, 1.0),
+    'clf__max_iter': (500, 1000, 1500),
+    # 'clf__penalty': ('l1', 'l2'),
+    # 'clf__C': (0.05, 0.1),
+    'clf__solver': ('newton-cg', 'lbfgs')
 }
 if tuning == "tuning":
     gsCV = GridSearchCV(text_clf, grid_params, verbose=3)
@@ -76,11 +86,11 @@ if tuning == "tuning":
     print("Best Score: ", gsCV.best_score_)
     print("Best Params: ", gsCV.best_params_)
 
-    with open('parameters_history.txt', 'a+') as f:
-        f.write("Tuning on: ", datetime.datetime.now(), '\n')
-        f.write(str(grid_params) + '\n')
-        f.write("Best Score: ", gsCV.best_score_, '\n')
-        f.write("Best Params: ", gsCV.best_params_, '\n\n\n')
+    # with open('parameters_history.txt', 'a+') as f:
+    #     f.write("Tuning on: ", datetime.datetime.now(), '\n')
+    #     f.write(str(grid_params) + '\n')
+    #     f.write("Best Score: ", gsCV.best_score_, '\n')
+    #     f.write("Best Params: ", gsCV.best_params_, '\n\n\n')
 # X_test = pd.DataFrame(dataset_analysis_extension(test_dataset, True))
 
 # clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X, y)
