66a9f539c0ffe942c6bd83eb4f2f2e2c93cc9a07
zoemcl <zoemcl@LAPTOP-1BLD0C7U.localdomain>
2019-10-10 00:45:01 -0400

    hyperparameter tuning semi functional

--- main.py
+++ main.py
@@ -1,20 +1,75 @@
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 import numpy as np
 import pandas as pd
 
-from dataset import load_dataset, dataset_analysis_extension
+from dataset import load_dataset, dataset_analysis_extension, clean_dataset
 
 # Load datasets
 
-original_dataset = load_dataset('reddit_train.csv', ',')
-# test_dataset = load_dataset('reddit_test.csv', ',')
+# Using CountVectorizer alone sometimes performes better than Tfif, so use either CountVectoriser or TfidfVectorizer?? Im confused
+count_vect = CountVectorizer()
+tfidf_transformer = TfidfTransformer()
 
-X = original_dataset.loc[:, original_dataset.columns != 'subreddits']
+original_dataset = load_dataset('reddit_train.csv', ',')
+test_dataset = load_dataset('reddit_test.csv', ',')
+
+X_orig = original_dataset.loc[:, original_dataset.columns != 'subreddits']
 y = original_dataset['subreddits']
 
-X = pd.DataFrame(dataset_analysis_extension(X, True))
+X_orig = clean_dataset(X_orig)
+X_test_pipe = clean_dataset(test_dataset)
+
+X = dataset_analysis_extension(X_orig, count_vect, tfidf_transformer)
+X_test = dataset_analysis_extension(X_test_pipe, count_vect, tfidf_transformer, test=True)
+
+clf = MultinomialNB()
+clf = clf.fit(X, y)
+
+text_clf = Pipeline([
+    ('vect', count_vect),
+    ('tfidf', tfidf_transformer),
+    ('mnb', MultinomialNB()),
+])
+
+grid_params = {
+  'mnb__alpha': np.linspace(0.5, 1.5, 6), #MultinomialNB param tuning
+  'mnb__fit_prior': [True, False]
+#   'tfidf__tfidf_transformer__max_df': np.linspace(0.1, 1, 10), # tfidf param tunings
+#   'tfidf__tfidf_transformer__binary': [True, False],
+#   'tfidf__tfidf_transformer__norm': [None, 'l1', 'l2']
+}
+
+clf = GridSearchCV(text_clf, grid_params)
+best_model = clf.fit(X_orig, y); # why X_orig
+
+print("Best Score: ", clf.best_score_)
+print("Best Params: ", clf.best_params_)
+
+preds = clf.predict(X_test_pipe)
+
+print(preds)
+
+# text_clf.fit(X_orig, y)
+
+# predicted = clf.predict(X_test)
+# predicted_pipeline = text_clf.predict(X_test_pipe)
+
+# with open('predictions.csv', 'w') as f:
+#     f.write("id,Category\n")
+#     for i, item in enumerate(predicted_pipeline):
+#         f.write(f"{ i },{ item }\n")
+
+# print(predicted, predicted_pipeline)
+
 # X_test = pd.DataFrame(dataset_analysis_extension(test_dataset, True))
 
 # clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X, y)
