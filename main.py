6f95703d8911f879768101c12f9a8145a211d0e2
evansavage <esav678@gmail.com>
2019-10-11 17:41:17 -0400

    Tweaking parameters

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ print('Is this run for tuning or for predicting?')
 tuning = input()
 # Load datasets
 
-count_vect = CountVectorizer(max_features=5000, ngram_range=(1,2))
+count_vect = CountVectorizer(ngram_range=(1,2), stop_words="english")
 tfidf_transformer = TfidfTransformer(use_idf=True, norm='l2')
 
 original_dataset = load_dataset('reddit_train.csv', ',')
@@ -37,7 +37,8 @@ X_orig = clean_dataset(X_orig)
 # X_test = dataset_analysis_extension(X_test_pipe, count_vect, tfidf_transformer, test=True)
 
 # print(X.shape, X_test.shape)
-
+# for x in X_orig:
+#     print(named_entry_recognition(x))
 
 # clf = MultinomialNB().fit(X, y)
 
@@ -47,26 +48,28 @@ text_clf = Pipeline([
             ('vect', count_vect),
             ('tfidf', tfidf_transformer),
         ])),
-        ('polarity', pipelinize_feature(get_polarity, active=True)),
-        ('subjectivity', pipelinize_feature(get_subjectivity, active=True)),
+        # ('polarity', pipelinize_feature(get_polarity, active=True)),
+        # ('subjectivity', pipelinize_feature(get_subjectivity, active=True)),
     ])),
-    ('clf', LogisticRegression(multi_class='multinomial')),
+    ('clf', MultinomialNB()),
 ])
 
 grid_params = {
-    'features__reg__vect__max_df': (0.5, 0.75, 1.0),
-    'features__reg__vect__min_df': (1,2,3),
-    # 'vect__max_features': (5000, 10000),
-    # 'tfidf__use_idf': (True, False),
-    # 'tfidf__norm': ('l1', 'l2'),
-    # 'clf__alpha': np.linspace(0.5, 1.5, 6), # For Naive Bayes
-    # 'clf__fit_prior': [True, False], # For Naive Bayes
+    'features__reg__vect__max_df': (0.2, 0.4, 0.6),
+    'features__reg__vect__min_df': (5, 10),
+    # 'features__reg__vect__max_features': (500000, 1000000),
+    # 'features__reg__tfidf__use_idf': (True, False),
+    # 'features__reg__tfidf__norm': ('l1', 'l2'),
+    'clf__alpha': np.linspace(0.5, 1.5, 6), # For Naive Bayes
+    'clf__fit_prior': [True, False], # For Naive Bayes
     # 'clf__decision_function_shape': ('ovo', 'ovr'), # For svm.SVC
-    'clf__max_iter': (500, 1000, 1500),
+    # 'clf__max_iter': (2000, 3000),
+    # 'clf__multi_class': ('ovr', 'crammer_singer'),
     # 'clf__penalty': ('l1', 'l2'),
-    # 'clf__C': (0.05, 0.1),
-    'clf__solver': ('newton-cg', 'lbfgs')
+    # 'clf__C': (0.8, 1.0),
+    # 'clf__solver': ('newton-cg', 'lbfgs')
 }
+
 if tuning == "tuning":
     gsCV = GridSearchCV(text_clf, grid_params, verbose=3)
 
@@ -83,8 +86,8 @@ if tuning == "tuning":
     #         f.write(f"{ i },{ item }\n")
 
     # print(predicted, predicted_pipeline)
-    print("Best Score: ", gsCV.best_score_)
-    print("Best Params: ", gsCV.best_params_)
+    # print("Best Score: ", gsCV.best_score_)
+    # print("Best Params: ", gsCV.best_params_)
 
     # with open('parameters_history.txt', 'a+') as f:
     #     f.write("Tuning on: ", datetime.datetime.now(), '\n')
