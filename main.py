2735f935be153237e0ef2ba9169cc906a0592889
evansavage <esav678@gmail.com>
2019-10-14 13:39:02 -0400

    Added input options for using existing clean dataset or saving new one

--- main.py
+++ main.py
@@ -8,39 +8,65 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import FeatureUnion, Pipeline
+from sklearn.decomposition import LatentDirichletAllocation
 from sklearn import svm
 import numpy as np
 import pandas as pd
 import datetime
+import csv
 
 from dataset import load_dataset, dataset_analysis_extension, clean_dataset, \
-    add_pol_sub, pipelinize_feature, named_entry_recognition, get_polarity, \
-    get_subjectivity
-
-print('Is this run for tuning or for predicting?')
+    add_pol_sub, pipelinize_feature, named_entity_recognition, get_polarity, \
+    get_subjectivity, ner_input, get_comment_length
+print('Use existing clean dataset? [y/n]')
+clean = input()
+print('Is this run for tuning or for predicting? [t/p]')
 tuning = input()
 # Load datasets
 
-count_vect = CountVectorizer(ngram_range=(1,2), stop_words="english")
+count_vect = CountVectorizer(stop_words="english")
 tfidf_transformer = TfidfTransformer(use_idf=True, norm='l2')
 
 original_dataset = load_dataset('reddit_train.csv', ',')
 test_dataset = load_dataset('reddit_test.csv', ',')
 
-X_orig = original_dataset.loc[:, original_dataset.columns != 'subreddits']
+X = original_dataset.loc[:, original_dataset.columns != 'subreddits']
 y = original_dataset['subreddits']
 
-X_orig = clean_dataset(X_orig)
-# X_test_pipe = clean_dataset(test_dataset)
+if clean == 'n':
+    X_orig = clean_dataset(X)
+    X_test_pipe = clean_dataset(test_dataset)
+    ner = named_entity_recognition(X_orig)
+    ner_test = named_entity_recognition(X_test_pipe)
 
-# X = dataset_analysis_extension(X_orig, count_vect, tfidf_transformer)
-# X_test = dataset_analysis_extension(X_test_pipe, count_vect, tfidf_transformer, test=True)
+    with open('clean_train.csv', 'w') as f:
+        f.write("train\n")
+        for entry in X_orig:
+            f.write(f"{ entry }\n")
 
-# print(X.shape, X_test.shape)
-# for x in X_orig:
-#     print(named_entry_recognition(x))
+    with open('clean_test.csv', 'w') as f:
+        f.write("test\n")
+        for entry in X_test_pipe:
+            f.write(f"{ entry }\n")
 
-# clf = MultinomialNB().fit(X, y)
+    ner.to_csv('ner_clean_train.csv')
+    ner_test.to_csv('ner_clean_test.csv')
+
+elif clean == 'y':
+    with open('clean_train.csv') as f:
+        X_orig = [line.strip() for line in f][1:]
+    with open('clean_test.csv') as f:
+        X_test_pipe = [line.strip() for line in f][1:]
+    ner = load_dataset('ner_clean_train.csv', ',').to_numpy()[:,1:]
+    ner_test = load_dataset('ner_clean_test.csv', ',').to_numpy()[:,1:]
+
+ner = ner / ner.max(axis=0)
+ner_test = ner_test / ner_test.max(axis=0)
+print(ner)
+
+lda = LatentDirichletAllocation(n_components=20, random_state=0)
+
+# lda.fit_transform(np.array(X_orig).reshape(-1,1), y)
 
 text_clf = Pipeline([
     ('features', FeatureUnion([
@@ -48,6 +74,13 @@ text_clf = Pipeline([
             ('vect', count_vect),
             ('tfidf', tfidf_transformer),
         ])),
+        ('topics', Pipeline([
+            ('vect', count_vect),
+            ('top', lda),
+        ])),
+        ('ner', ner_input(ner, ner_test, active=True)),
+        ('len', pipelinize_feature(get_comment_length, active=True)),
+
         # ('polarity', pipelinize_feature(get_polarity, active=True)),
         # ('subjectivity', pipelinize_feature(get_subjectivity, active=True)),
     ])),
@@ -55,9 +88,14 @@ text_clf = Pipeline([
 ])
 
 grid_params = {
-    'features__reg__vect__max_df': (0.2, 0.4, 0.6),
-    'features__reg__vect__min_df': (5, 10),
-    # 'features__reg__vect__max_features': (500000, 1000000),
+    'features__reg__vect__max_df': (0.8, 0.9, 1),
+    'features__reg__vect__min_df': (5,10,12),
+    # 'features__reg__vect__ngram_range': ((1,2),(1,3),(1,4),(1,5),(1,6)),
+    # 'features__reg__vect__max_features': (10, 20),
+    # 'features__topics__vect__max_features': (20000, 40000),
+    # 'features__topics__vect__max_df': (0.8, 0.9, 1),
+    # 'features__topics__vect__min_df': (5,10,12),
+    'features__topics__vect__ngram_range': ((1,2),(1,3),(1,4),(1,5),(1,6)),
     # 'features__reg__tfidf__use_idf': (True, False),
     # 'features__reg__tfidf__norm': ('l1', 'l2'),
     'clf__alpha': np.linspace(0.5, 1.5, 6), # For Naive Bayes
@@ -70,24 +108,33 @@ grid_params = {
     # 'clf__solver': ('newton-cg', 'lbfgs')
 }
 
-if tuning == "tuning":
+if tuning == "t":
     gsCV = GridSearchCV(text_clf, grid_params, verbose=3)
 
     # text_clf.fit(X_orig, y)
 
     gsCV.fit(X_orig, y)
-
-    # predicted = clf.predict(X_test)
+    print("Best Score: ", gsCV.best_score_)
+    print("Best Params: ", gsCV.best_params_)
+    # predicted = gsCV.predict(X_test)
     # predicted_pipeline = text_clf.predict(X_test_pipe)
-
-    # with open('predictions.csv', 'w') as f:
-    #     f.write("id,Category\n")
-    #     for i, item in enumerate(predicted_pipeline):
-    #         f.write(f"{ i },{ item }\n")
+elif tuning == 'p':
+    text_clf.set_params(
+        features__reg__vect__max_df=0.8,
+        features__reg__vect__min_df=5,
+        features__topics__vect__ngram_range=(1, 2),
+        clf__alpha=0.5,
+        clf__fit_prior=True,
+    )
+    text_clf.fit(X_orig, y)
+    predicted = text_clf.predict(X_test_pipe)
+    with open('predictions.csv', 'w') as f:
+        f.write("id,Category\n")
+        for i, item in enumerate(predicted):
+            f.write(f"{ i },{ item }\n")
 
     # print(predicted, predicted_pipeline)
-    # print("Best Score: ", gsCV.best_score_)
-    # print("Best Params: ", gsCV.best_params_)
+
 
     # with open('parameters_history.txt', 'a+') as f:
     #     f.write("Tuning on: ", datetime.datetime.now(), '\n')
