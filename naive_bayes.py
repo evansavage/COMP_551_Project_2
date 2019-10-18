a534f2b2a6c85c894a88b12a73531b9e3c26ba29
suzuka_kokubu <suzuka.kokubu@mail.mcgill.ca>
2019-10-18 13:10:02 -0400

    optimized

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,50 +1,62 @@
 import numpy as np
 import pandas as pd
 from scipy.sparse import coo_matrix, hstack, csr_matrix
-
+from sklearn import preprocessing
 
 class NaiveBayes:
     def __init__(self):
         self.classes = []
         self.num_features = 0
         self.theta_k = []
-        self.theta_jk = []
-        prediction = []
+        self.encoder = preprocessing.LabelEncoder()
 
-    def train(self, X, y):
+    def fit(self, X, y):
         self.classes = np.unique(y)
+        self.encoder.fit(y)
         self.num_features = X.getrow(0).shape[1]
         self.theta_k = [] #marginal probability for each class
-        self.theta_jk = [] #confitional probability of each feature given each class [each class[each features]]
+        self.theta_jk = np.empty((len(self.encoder.classes_), X.shape[1])) #confitional probability of each feature given each class [each class[each features]]
+        print(self.theta_jk.shape)
+        y = np.array(y)
 
-        for k in self.classes:
-            t_k = y[y == k].count()/ y.count() # (# of examples where y=1) / (total # of examples)
-            X_k = X.toarray()[np.where(np.array(y) == k)]
-            #X_k = X.toarray()[y[y == k].index]
+        num_examples = len(y)
+        for i, k in enumerate(self.encoder.classes_):
+            length_y = len(y[y == k])
+            t_k = length_y / num_examples # (# of examples where y=1) / (total # of examples)
+            X_k = X[np.where(y == k)]
             self.theta_k.append(t_k)
-            t_jk = []
-            for i in range(self.num_features):
-                tjk = (sum(X_k[:, i])+1) /(y[y == k].count()+2) #(# of examples where xj=1 and y=k) / (# of examples where y=k)
-                t_jk.append(tjk)
-
-            self.theta_jk.append(t_jk)
-
-    def predict(self, X):
+            sum_array = np.sum(X_k, axis=0)
+            t_jk = (sum_array + 1) / (length_y + 2)
+            self.theta_jk[i] = t_jk
+        return self
+   
+    def predict(self, X, y=None):
+        X = X.toarray()
+        print("predict start")
+        print(len(X[0]), self.num_features, len(self.theta_jk[0]))
         prediction = []
         for i in range(len(X)):
             class_prob = []
             for k in range(len(self.classes)):
-                feature_likelihood = 0
-                for j in range(num_features):
-                    feature_likelihood += x[j]*np.log(theta_jk[k][j]+(1-x[j])*np.log(1-theta_jk[k][j]))
-                cb = feature_likelihood+np.log(theta_k[k])
+                
+                feature_likelihood = np.dot(X[i],np.log(self.theta_jk[k])) + np.dot((1-X[i]), np.log(1-self.theta_jk[k]))
+               
+                cb = feature_likelihood+np.log(self.theta_k[k])
                 class_prob.append(cb)
             prediction.append(np.argmax(class_prob))
         return prediction
 
-    def validation(self, prediction, y):
-        correct = 0
-        for i in range(len(prediction)):
-            correct += 1 if np.unique(y)[prediction[i]] == y[i] else 0
-        accuracy = correct / len(prediction)
-        return accuracy
+    def score(self, X, y=None):
+        prediction = self.predict(X)
+        correct = sum(np.equal(self.classes[prediction], y))
+        return correct / len(prediction)
+
+    # def validation(self, prediction, y):
+    #     # print("lenth of prediction ", self.classes[prediction])
+    #     # print("lenth of y ", y)
+    #     correct = sum(np.equal(self.classes[prediction], y))
+    # #    c = np.logical_and(prediction == y)
+    #     # for i in range(len(prediction)):
+    #     #     correct += 1 if y[prediction[i]] == y[i] else 0
+    #     accuracy = correct / len(prediction)
+    #     return accuracy
