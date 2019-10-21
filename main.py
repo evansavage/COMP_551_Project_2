dd437673b207604d529564055f6d4f4d0827c24e
evansavage <esav678@gmail.com>
2019-10-20 22:26:49 -0400

    Cleaning up main.py

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_selection import RFE
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import FeatureUnion, Pipeline
-from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn import svm
 import numpy as np
@@ -29,17 +27,24 @@ tuning = input()
 
 count_vect = CountVectorizer(stop_words="english")
 tfidf_transformer = TfidfTransformer(use_idf=True, norm='l2')
-tfidf_vec = TfidfVectorizer(stop_words="english")
+tfidf_vec = TfidfVectorizer()
+scaler = StandardScaler()
 
 original_dataset = load_dataset('reddit_train.csv', ',')
 test_dataset = load_dataset('reddit_test.csv', ',')
+new_orig_dataset = load_dataset('reddit_train_updated.csv', ',').dropna()
+print(new_orig_dataset)
 
 X = original_dataset.loc[:, original_dataset.columns != 'subreddits']
 y = original_dataset['subreddits']
 
+X_new = new_orig_dataset.comments.values
+y_new = new_orig_dataset.subreddits.values
+print(new_orig_dataset.subreddits.unique())
 if clean == 'n':
     X_orig = clean_dataset(X)
     X_test_pipe = clean_dataset(test_dataset)
+    # perform named entity recognition
     ner = named_entity_recognition(X_orig)
     ner_test = named_entity_recognition(X_test_pipe)
 
@@ -63,24 +68,22 @@ elif clean == 'y':
         X_test_pipe = [line.strip() for line in f][1:]
     ner = load_dataset('ner_clean_train.csv', ',').to_numpy()[:,1:]
     ner_test = load_dataset('ner_clean_test.csv', ',').to_numpy()[:,1:]
-
+elif clean == 'w':
+    # Use extended dataset with new comments
+    X_orig = X.comments.values
+    # y = y_new
+    X_test_pipe = test_dataset.values
+    ner = load_dataset('ner_clean_train.csv', ',').to_numpy()[:,1:]
+    ner_test = load_dataset('ner_clean_test.csv', ',').to_numpy()[:,1:]
 ner = ner / ner.max(axis=0)
 ner_test = ner_test / ner_test.max(axis=0)
-print(ner)
-
-lda = LatentDirichletAllocation(n_components=20, random_state=0)
-rfe = RFE(MultinomialNB())
-# lda.fit_transform(np.array(X_orig).reshape(-1,1), y)
+print(type(ner), type(ner_test))
 
 text_clf = Pipeline([
     ('features', FeatureUnion([
-        # ('reg', Pipeline([
-        #     ('vect', count_vect),
-        #     ('tfidf', tfidf_transformer),
-        # ])),
         ('reg', Pipeline([
             ('tfvec', tfidf_vec)
-        ]))
+        ])),
         # ('topics', Pipeline([
         #     ('vect', count_vect),
         #     ('top', lda),
@@ -90,17 +93,16 @@ text_clf = Pipeline([
         # ('polarity', pipelinize_feature(get_polarity, active=True)),
         # ('subjectivity', pipelinize_feature(get_subjectivity, active=True)),
     ])),
-    # ('rfe', rfe),
-    ('clf', svm.LinearSVC(fit_intercept=True)),
+    ('clf', LogisticRegression()),
 ])
 
 grid_params = {
     # 'features__reg__vect__max_df': (0.1,0.9, 1),
     # 'features__reg__tfvec__min_df': (2,3),
     # 'features__reg__vect__ngram_range': ((1,2),(1,3)),
-    'features__reg__tfvec__max_df': (0.1, 0.9, 1),
-    'features__reg__tfvec__ngram_range': ((1,2),(1,3)),
-    # 'features__reg__tfvec__sublinear_tf': (True, False),
+    'features__reg__tfvec__max_df': (0.8, 0.9),
+    # 'features__reg__tfvec__ngram_range': ((1,3),(1,2)),
+    'features__reg__tfvec__sublinear_tf': (True, False),
     # 'features__reg__tfvec__norm': ('l1', 'l2'),
     # 'features__reg__tfvec__max_features': (800000, 1200000),
     # 'features__topics__vect__max_features': (1500, 3000),
@@ -112,27 +114,35 @@ grid_params = {
     # 'clf__alpha': np.linspace(1, 1.5, 6), # For Naive Bayes
     # 'clf__fit_prior': [True, False], # For Naive Bayes
     # 'clf__decision_function_shape': ('ovo', 'ovr'), # For svm.SVC
-    'clf__max_iter': (1100, 5000),
-    'clf__intercept_scaling': (1.5, 1.8),
+    # 'clf__max_iter': np.arange(600, 6000, 600),
+    # 'clf__fit_intercept': (True, False),
+    # 'clf__intercept_scaling': (0.1, 2.0, 0.2),
     # 'clf__multi_class': ('ovr', 'crammer_singer'),
+    # 'clf__dual': (True, False),
     # 'clf__penalty': ('l1', 'l2'),
-    # 'clf__C': (1.3, 0.9),
+    # 'clf__C': np.arange(0.2, 2.0, 0.2),
+    # 'clf__tol': np.arange(0.00002, 0.0002, 0.00006),
     # 'clf__solver': ('newton-cg', 'lbfgs'),
     # 'clf__n_estimators': (10, 100),
-    # 'clf__random_state': (0,1),
+    # 'clf__random_state': (0,1,2),
     # 'clf__max_features': ('auto', 30, 60),
+    # 'clf__learning_rate': ('optimal', 'optimal'),
+    # 'clf__eta0': (1,1),
+    # 'clf__average': (1, 5, 10,20),
+    # 'clf__epsilon': (0.5, 1),
+    # 'clf__loss': ('modified_huber', 'huber'),
+    # 'clf__alpha': (0.00001, 0.0001, 0.001),
 }
 
+# Use gridsearch to tune the model
 if tuning == "t":
     gsCV = GridSearchCV(text_clf, grid_params, verbose=3)
-
-    # text_clf.fit(X_orig, y)
 
     gsCV.fit(X_orig, y)
     print("Best Score: ", gsCV.best_score_)
     print("Best Params: ", gsCV.best_params_)
-    # predicted = gsCV.predict(X_test)
-    # predicted_pipeline = text_clf.predict(X_test_pipe)
+    predicted = gsCV.predict(X_test_pipe)
+# Predict with optimal parameters
 elif tuning == 'p':
     text_clf.set_params(
         features__reg__tfvec__max_df=0.1,
@@ -145,22 +155,8 @@ elif tuning == 'p':
     )
     text_clf.fit(X_orig, y)
     predicted = text_clf.predict(X_test_pipe)
-    with open('predictions.csv', 'w') as f:
-        f.write("id,Category\n")
-        for i, item in enumerate(predicted):
-            f.write(f"{ i },{ item }\n")
 
-    # print(predicted, predicted_pipeline)
-
-
-    # with open('parameters_history.txt', 'a+') as f:
-    #     f.write("Tuning on: ", datetime.datetime.now(), '\n')
-    #     f.write(str(grid_params) + '\n')
-    #     f.write("Best Score: ", gsCV.best_score_, '\n')
-    #     f.write("Best Params: ", gsCV.best_params_, '\n\n\n')
-# X_test = pd.DataFrame(dataset_analysis_extension(test_dataset, True))
-
-# clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X, y)
-
-# print(updated_dataset)
-# print(updated_dataset.shape)
+with open('predictions.csv', 'w') as f:
+    f.write("id,Category\n")
+    for i, item in enumerate(predicted):
+        f.write(f"{ i },{ item }\n")
